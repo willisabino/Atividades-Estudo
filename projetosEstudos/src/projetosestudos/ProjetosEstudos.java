9ae57949b31884f670e89dafc4638f58ac284640
willisabino <https://github.com/willisabino>
2022-05-03 10:57:52 -0300

    Commit em um novo ramo

--- projetosEstudos/src/projetosestudos/ProjetosEstudos.java
+++ projetosEstudos/src/projetosestudos/ProjetosEstudos.java
@@ -6,7 +6,8 @@ public class ProjetosEstudos {
 		System.out.println("Hello World");
 		
 		System.out.println("Estou adicionando coisas para estudar o sourceTree");
-
+		
+		System.out.println("Acabei de criar um novo ramo e estou testando");
 	}
 
 }
