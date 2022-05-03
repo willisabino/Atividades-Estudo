7cfd260f39658c24f47b32a1bbbfc10d1acc2f97
willisabino <https://github.com/willisabino>
2022-05-03 11:00:00 -0300

    Commitando uma nova branch

--- projetosEstudos/src/projetosestudos/ProjetosEstudos.java
+++ projetosEstudos/src/projetosestudos/ProjetosEstudos.java
@@ -8,6 +8,8 @@ public class ProjetosEstudos {
 		System.out.println("Estou adicionando coisas para estudar o sourceTree");
 		
 		System.out.println("Acabei de criar um novo ramo e estou testando");
+		
+		System.out.println("Ramo do teste");
 	}
 
 }
