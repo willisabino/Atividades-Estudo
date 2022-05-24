b7b479cf502c473eb2e7bb090a372a37eb2a5bec
willisabino <https://github.com/willisabino>
2022-05-24 18:18:47 -0300

    Estudos de StringBuilder

---
+++ StringBuilder/src/br/com/springbuilder/Delete.java
@@ -0,0 +1,24 @@
+package br.com.springbuilder;
+
+public class Delete {
+
+	public static void main(String[] args) {
+		Object objetoRef = "Maria";
+		String String = "Joaquina";
+		char[] conjArray = { 'S', 'i', 'l', 'v', 'a' };
+
+		StringBuilder buffer = new StringBuilder();
+
+		buffer.insert(0, objetoRef);
+		buffer.insert(0, " ");
+		buffer.insert(0, String);
+		buffer.insert(0, " ");
+		buffer.insert(0, conjArray);
+		buffer.insert(0, " ");
+		System.out.printf("Valores antes do delete:\n%s\n\n", buffer.toString());
+
+		buffer.delete(0, 2);
+		buffer.deleteCharAt(4);
+		System.out.printf("Valores depois do delete:\n%s\n\n", buffer.toString());
+	}
+}
---
+++ StringBuilder/src/br/com/springbuilder/Insert.java
@@ -0,0 +1,20 @@
+package br.com.springbuilder;
+
+public class Insert {
+
+	public static void main(String[] args) {
+		Object objetoRef = "Maria";
+		String String = "Joaquina";
+		char[] conjArray = { 'S', 'i', 'l', 'v', 'a' };
+
+		StringBuilder buffer = new StringBuilder();
+		buffer.insert(0, objetoRef);
+		buffer.insert(0, " ");
+		buffer.insert(0, String);
+		buffer.insert(0, " ");
+		buffer.insert(0, conjArray);
+		buffer.insert(0, " ");
+
+		System.out.printf("Valores:\n%s\n\n", buffer.toString());
+	}
+}
